c034fd51da29865245d80fa26afc070b856d176b
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2017-12-14 14:11:38 -0500

    Added Plain SpringBoot Project

---
+++ skunkworks-demo/src/main/java/com/ihg/skunkworks/SkunkworksDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ihg.skunkworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkunkworksDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkunkworksDemoApplication.class, args);
+	}
+}
